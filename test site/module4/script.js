4c5b41e02c9df2c3e18080a6195a6caa5429033b
shannadelea <164967109+shannadelea@users.noreply.github.com>
2024-04-18 18:52:07 -0500

    Update script.js

--- test site/module4/script.js
+++ test site/module4/script.js
@@ -10,15 +10,12 @@ names[7]="paula";
 names[8]="laura";
 names[9]="jim";
 
+
 for (var i = 0; i < names.length; i++) {
-  var firstLetter = names[i].charAt(0).toLowerCase();
-
-  if (firstLetter === 'j') {
-    byeSpeaker(names[i]);
-  }
-  else {
-    helloSpeaker(names[i]);
-  }
+	if(names[i].charAt(0)==='J'|| names[i].charAt(0)==='j'){
+        console.log("Goodbye "+ names[i])
+	}
+	else{
+		console.log("Hello "+ names[i])
+	}
 }
-
-})();
