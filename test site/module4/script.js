e3ae3e7a0dc5ded4c69069c2762e8f0cc68647a3
shannadelea <164967109+shannadelea@users.noreply.github.com>
2024-04-18 18:39:44 -0500

    Update script.js

--- test site/module4/script.js
+++ test site/module4/script.js
@@ -10,11 +10,11 @@ names[7]="paula";
 names[8]="laura";
 names[9]="jim";
 
-for (var i = 0; 1 < names.length; i++) {
+for (var i = 0; i < names.length; i++) {
   if(names[i].charAt(0)==='J' || names[i].charAt(0)==='j') {
     console.log("Goodbye " +name [i])
     }
     else{
       console.log("Hello "+ names[i]) 
     }
-}
+  }
